65d74e1de902b47a54666038b0c17773023f05fe
Nikitarama <https://github.com/Nikitarama/MyPortfolio.git>
2023-02-27 16:49:20 +0200

    second

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="home">
-    
+  <Home/>
+  <div class="body">
+      <img src="https://i.postimg.cc/C5jfF6M7/brownie-gc671efb18-1920.jpg">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Home from '@/components/NavbarComp.vue'
+// import Home from '@/components/NavbarComp.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    Home
-  }
-}
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     Home
+//   }
+// }
 </script>
+
+<style scoped>
+
+.body img {
+  height: 50rem;
+  width: 70rem;
+  
+}
+
+
+</style>
