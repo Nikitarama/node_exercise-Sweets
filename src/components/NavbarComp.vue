532efda1d3a600b35ad34c3651c35ae7ed1e1713
Nikitarama <https://github.com/Nikitarama/MyPortfolio.git>
2023-02-27 16:47:05 +0200

    second

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="nav">
+    <!-- <ul class="nav">
   <li class="nav-item">
     <router-link to="/">Home
 
@@ -24,7 +24,68 @@
   <li class="nav-item">
     <a class="nav-link disabled">Disabled</a>
   </li>
+</ul> -->
+  <div class="nav justify-content-start">
+    <img src="https://i.postimg.cc/13wd1TXf/LOGO-Sweet-NC.png" style="width:200px;">
+  </div>
+
+<ul class="nav justify-content-center">
+    
+  <li class="nav-item">
+    <router-link to="/"> Home
+
+</router-link> | 
+    
+  </li>
+  <li class="nav-item">
+    <router-link to="/about"> About
+
+</router-link> | 
+    
+  </li>
+  <li class="nav-item">
+    <router-link to="/admin"> Admin
+      
+</router-link> |
+    
+  </li>
 </ul>
 
 
 </template>
+
+
+
+<script>
+
+</script>
+
+
+
+
+<style scoped>
+
+* {
+  margin: 0;
+}
+
+.nav-item {
+  padding-right: 30px;
+  margin-bottom: 30px;
+  
+}
+
+
+.nav {
+  color:antiquewhite;
+  text-decoration: none;
+  list-style: none;
+
+}
+
+.nav {
+  background-color: #be8a62;
+  
+}
+
+</style>
